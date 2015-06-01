e1a5a2702f1b5d5f5f4cb2d8fc7d7939ec801ba9
hasankryeziu <hasan_kryeziu123@hotmail.com>
2015-06-01 22:08:07 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,41 @@
+Module OXID Eshop CE 4.8.x (PE/CE) ShopLocator StoreLocator
+============================================================
+
+
+The module ShopLocator for Oxid Eshop CE 4.8.x (PE/CE)
+
+Version: 1.0.1 :
+
+language german
+
+streetview and styled maps
+
+configuration in the backend
+
+google Maps with Sidebar and Filter
+
+additional Information
+
+geocoordinates will be genrate from the adress
+
+individuel markers
+
+calculate routes 
+
+
+===========================================================
+
+Backend:
+
+![ScreenShot](https://raw.githubusercontent.com/hasankryeziu/OXID_EShop_ShopLocator/master/screenshots/backend1.png)
+
+![ScreenShot1](https://raw.githubusercontent.com/hasankryeziu/OXID_EShop_ShopLocator/master/screenshots/backend2.png)
+
+Frontend:
+
+![ScreenShot2](https://raw.githubusercontent.com/hasankryeziu/OXID_EShop_ShopLocator/master/screenshots/frontend.png)
+
+
+Install the module: use the install Folder.
+
+
---
+++ metadata.php
@@ -0,0 +1,36 @@
+	<?php
+$aModule = array(
+    'id' => 'shoplocator',
+    'title' => 'Shop Locator',
+    'description' => 'Shop Locator for Oxid CE/PE 4.8.x with Google Api V3 .',
+    'thumbnail' => 'logo.png',
+    'version' => '1.0.0 alpha',
+    'author' => 'Hasan Kryeziu',
+    'github' => 'hasankryeziu',
+    'email' => 'hasan_kryeziu123@hotmail.com',
+    'extend' => array(),
+    'files' => array(
+        'admin_shoplocator' => 'shoplocator/application/controllers/admin/admin_shoplocator.php',
+        'admin_shoplocator_main' => 'shoplocator/application/controllers/admin/admin_shoplocator_main.php',
+        'admin_shoplocator_list' => 'shoplocator/application/controllers/admin/admin_shoplocator_list.php',
+        'admin_shoplocator_configuration' => 'shoplocator/application/controllers/admin/admin_shoplocator_configuration.php',
+        'shoplocator_list' => 'shoplocator/application/models/shoplocator_list.php',
+        'shoplocator' => 'shoplocator/application/models/shoplocator.php',
+        'shoplocator_settings' => 'shoplocator/application/models/shoplocator_settings.php',
+        'shoplocator_out' => 'shoplocator/application/controllers/shoplocator_out.php',
+    ),
+    'templates' => array(
+        'admin_shoplocator.tpl' => 'shoplocator/application/views/admin/tpl/admin_shoplocator.tpl',
+        'admin_shoplocator_main.tpl' => 'shoplocator/application/views/admin/tpl/admin_shoplocator_main.tpl',
+        'admin_shoplocator_list.tpl' => 'shoplocator/application/views/admin/tpl/admin_shoplocator_list.tpl',
+        'admin_shoplocator_configuration.tpl' => 'shoplocator/application/views/admin/tpl/admin_shoplocator_configuration.tpl',
+        'shoplocator_out.tpl' => 'shoplocator/application/views/shoplocator_out.tpl',
+    ),
+    'blocks' => array(
+        array(
+            'template' => 'layout/sidebar.tpl',
+            'block' => 'sidebar',
+            'file' => 'out/blocks/sidebar.tpl'
+        ),
+    )
+);
